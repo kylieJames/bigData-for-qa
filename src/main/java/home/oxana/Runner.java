35caeed4fb2dac46f55cb27124988d90810a7fc7
oxana <kaila@inbox.ru>
2018-03-11 09:11:17 -0400

    Jenkins code

---
+++ src/main/java/home/oxana/Runner.java
@@ -0,0 +1,14 @@
+package home.oxana;
+
+/**
+ * Hello world!
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(getStringToPrint());
+    }
+
+    static String getStringToPrint() {
+        return "Hello world!";
+    }
+}
---
+++ src/test/java/home/oxana/RunnerTest.java
@@ -0,0 +1,21 @@
+package home.oxana;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static home.oxana.Runner.getStringToPrint;
+import static org.hamcrest.CoreMatchers.is;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class RunnerTest {
+    private static final String TEST_STRING = "Hello world!";
+
+    @Test
+    public void testHelloWorld() {
+        Assert.assertThat(getStringToPrint(), is(TEST_STRING));
+    }
+}
